420f66895f64476234756411f5686bfc7106daa9
oszegda <oszegda@icloud.com>
2022-12-06 23:38:29 +0100

    Learned Other Useful Building Blocks from chapter 3

--- chapter_3.rb
+++ chapter_3.rb
@@ -172,3 +172,76 @@ each_vowel { |vowel| puts vowel}
 
 print_parameter_to_screen = Proc.new { |x| puts x }
 print_parameter_to_screen.call(100)
+
+# Other Useful Building Blocks
+
+puts Time.now
+puts Time.now - 10
+puts Time.now + 86400
+
+class Integer
+  def seconds
+    self
+  end
+  def minutes
+    self * 60
+  end
+  def hours
+    self * 60 * 60
+  end
+  def days
+    self * 60 * 60 * 24
+  end
+end
+
+puts Time.now
+puts Time.now + 10.minutes
+puts Time.now + 16.hours
+puts Time.now - 7.days
+
+year = 2020
+month = 1
+day = 16
+hour = 12
+min = 57
+sec = 10
+msec = 42
+puts Time.local(year, month, day, hour, min, sec, msec)
+puts Time.gm(year, month, day, hour, min, sec, msec)
+puts Time.utc(year, month, day, hour, min, sec, msec)
+puts Time.gm(2020, 02).to_i
+
+epoch_time = Time.gm(2020, 2).to_i
+t = Time.at(epoch_time)
+puts t.year, t.month, t.day
+
+puts Time.now.hour
+puts Time.now.min
+puts Time.now.sec
+puts Time.now.usec
+puts Time.now.day
+puts Time.now.mday
+puts Time.now.wday
+puts Time.now.yday
+puts Time.now.month
+puts Time.now.year
+puts Time.now.zone
+puts Time.now.utc?
+puts Time.now.gmt?
+
+('A'..'Z').each do |letter|
+  puts letter
+end
+
+puts ('A'..'Z').include?('R')
+puts ('A'..'Z').include?('r')
+
+a = [2, 4, 6, 8, 10, 12]
+p a[1..3]
+
+a[1..3] = ['a', 'b', 'c']
+p a
+
+current_situation = :good
+puts "Everything is fine" if current_situation == :good
+puts "PANIC!" if current_situation == :bad
