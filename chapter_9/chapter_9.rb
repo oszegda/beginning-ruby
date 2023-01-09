38e2c8d6349e4627d8fa72fa40338f383322c9a4
oszegda <oszegda@icloud.com>
2023-01-10 00:12:07 +0100

    Files and Database in Chapter 9 - Renaming and Deleting Files

--- chapter_9/chapter_9.rb
+++ chapter_9/chapter_9.rb
@@ -108,3 +108,19 @@ f.close
 f = File.open('chapter_9/text_3.txt', 'r+')
 f.write '123456'
 f.close
+
+# Character Sets and Encoding
+
+File.new('text.txt', 'r:utf-8').each { |line| puts line}
+
+p File.open('text.txt', 'r:iso-8859-1').external_encoding
+p File.open('text.txt', 'r').external_encoding
+
+File.open('text.txt', 'r:utf-8:iso-8859-1') do |f|
+  p f.external_encoding
+  first_line = f.gets
+  p first_line.encoding
+end
+
+# Renaming and Deleting Files
+
